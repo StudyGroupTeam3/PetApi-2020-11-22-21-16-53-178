1802bf0882e8fdaee29b8c6fbe92293fb3c543df
Ziyuan <zxu25@slb.com>
2020-11-23 18:31:46 +0800

    feat: US1 AC2 add GET /PetStore/Pets to get all pets in store

--- PetApiTest/PetApiTest.cs
+++ PetApiTest/PetApiTest.cs
@@ -1,12 +1,15 @@
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Newtonsoft.Json;
 using PetApi;
+using PetApi.Controllers;
 using Xunit;
 
 namespace PetApiTest
@@ -29,6 +32,26 @@ namespace PetApiTest
             var responseString = await response.Content.ReadAsStringAsync();
             Pet actualPet = JsonConvert.DeserializeObject<Pet>(responseString);
             Assert.Equal(pet, actualPet);
+        }
+
+        [Fact]
+        public async Task Should_Return_All_Pets_When_Get_All_Pets()
+        {
+            // given
+            TestServer testServer = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            HttpClient client = testServer.CreateClient();
+            await client.DeleteAsync("petStore/clear");
+            Pet pet = new Pet("Baymax", "dog", "white", 5000);
+            string request = JsonConvert.SerializeObject(pet);
+            StringContent requestBody = new StringContent(request, Encoding.UTF8, "application/json");
+            await client.PostAsync("petStore/addNewPet", requestBody);
+            // when
+            var response = await client.GetAsync("petStore/pets");
+            // then
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            var actualPets = JsonConvert.DeserializeObject<List<Pet>>(responseString);
+            Assert.Equal(new List<Pet>() { pet }, actualPets);
         }
     }
 }
