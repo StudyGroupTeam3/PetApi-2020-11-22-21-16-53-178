ee5e73289d761ef3c3fec7f1dfcc724cbe413a79
Ying Fu <YFu3@slb.com>
2020-11-23 16:13:43 +0800

    feat: Add pet when given pet

---
+++ PetApi/Pet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetApi
+{
+    public class Pet
+    {
+        public Pet()
+        {
+        }
+
+        public Pet(string name, string type, string color, int price)
+        {
+            Name = name;
+            Type = type;
+            Color = color;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+        public int Price { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals((Pet)obj);
+        }
+
+        public bool Equals(Pet pet)
+        {
+            return Name == pet.Name && Type == pet.Type && Color == pet.Color && Price == pet.Price;
+        }
+    }
+}
---
+++ PetApiTest/PetApiTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using PetApi;
+using Xunit;
+
+namespace PetApiTest
+{
+    public class PetApiTest
+    {
+        [Fact]
+        public async Task Should_Add_Pet_When_Add_Pet()
+        {
+            // given
+            TestServer server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+            HttpClient client = server.CreateClient();
+
+            Pet pet = new Pet(name: "Baymas", type: "dog", color: "White", price: 5000);
+            string request = JsonConvert.SerializeObject(pet);
+            StringContent requestBody = new StringContent(request, Encoding.UTF8, "application/json");
+
+            // when
+            var response = await client.PostAsync("PetStore/addNewPet", requestBody);
+
+            // then
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            Pet acutalPet = JsonConvert.DeserializeObject<Pet>(responseString);
+            Assert.Equal(pet, acutalPet);
+        }
+
+
+    }
+}
