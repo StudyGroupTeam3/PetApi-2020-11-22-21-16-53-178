7c14a31d77f641f7ba5508b42eeb379107c5cb6e
Ying Fu <YFu3@slb.com>
2020-11-23 19:06:00 +0800

    feat: find pet by its name

--- PetApi/Controllers/PetStoreController.cs
+++ PetApi/Controllers/PetStoreController.cs
@@ -11,12 +11,32 @@ namespace PetApi.Controllers
     [Route("[controller]")]
     public class PetStoreController : ControllerBase
     {
-        private List<Pet> pets = new List<Pet>();
+        private static List<Pet> pets = new List<Pet>();
         [HttpPost("addNewPet")]
         public Pet AddPet(Pet pet)
         {
             pets.Add(pet);
             return pet;
+        }
+
+        [HttpGet("pets")]
+        public IEnumerable<Pet> GetAllPets()
+        {
+            return pets;
+        }
+
+        [HttpGet]
+        [Route("{name?}")]
+        //[Route("FindPetByItsName/")]
+        public Pet GetPetByItsName(string name)
+        {
+            return pets.FirstOrDefault(x => x.Name == name);
+        }
+
+        [HttpDelete("clear")]
+        public void Clear()
+        {
+            pets.Clear();
         }
     }
 }
--- PetApiTest/PetApiTest.cs
+++ PetApiTest/PetApiTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -16,7 +17,6 @@ namespace PetApiTest
         {
             // given
             TestServer server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
-
             HttpClient client = server.CreateClient();
 
             Pet pet = new Pet(name: "Baymas", type: "dog", color: "White", price: 5000);
@@ -33,6 +33,53 @@ namespace PetApiTest
             Assert.Equal(pet, acutalPet);
         }
 
+        [Fact]
+        public async Task Should_Return_Correct_Pets_When_Get_All_Pets()
+        {
+            // given
+            TestServer server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            HttpClient client = server.CreateClient();
+            await client.DeleteAsync("petStore/clear");
 
+            // when
+            Pet pet = new Pet(name: "Baymas", type: "dog", color: "White", price: 5000);
+            string request = JsonConvert.SerializeObject(pet);
+            StringContent requestBody = new StringContent(request, Encoding.UTF8, "application/json");
+            await client.PostAsync("PetStore/addNewPet", requestBody);
+
+            var response = await client.GetAsync("PetStore/pets");
+
+            // then
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            List<Pet> acutalPet = JsonConvert.DeserializeObject<List<Pet>>(responseString);
+            Assert.Equal(new List<Pet>() { pet }, acutalPet);
+        }
+
+        [Fact]
+        public async Task Should_Find_Pet_When_Give_Pet_Name()
+        {
+            // given
+            TestServer server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            HttpClient client = server.CreateClient();
+
+            Pet pet1 = new Pet(name: "Baymas", type: "dog", color: "White", price: 5000);
+            Pet pet2 = new Pet(name: "Apple", type: "cat", color: "White", price: 5000);
+            string request1 = JsonConvert.SerializeObject(pet1);
+            string request2 = JsonConvert.SerializeObject(pet2);
+            StringContent requestBody1 = new StringContent(request1, Encoding.UTF8, "application/json");
+            StringContent requestBody2 = new StringContent(request2, Encoding.UTF8, "application/json");
+            await client.PostAsync("PetStore/addNewPet", requestBody1);
+            await client.PostAsync("PetStore/addNewPet", requestBody2);
+
+            // when
+            //var response = await client.GetAsync("PetStore/FindPetByItsName?name=Baymas");
+            var response = await client.GetAsync("PetStore/Baymas");
+            // then
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            Pet acutalPet = JsonConvert.DeserializeObject<Pet>(responseString);
+            Assert.Equal(pet1,  acutalPet);
+        }
     }
 }
